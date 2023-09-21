3293318ac04f8edb38794e39bcb86e5e7de7b514
STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>
2023-09-21 13:00:08 +0100

    Signed-off-by: STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>

--- GD2023/Assets/Rob/Scripts/CharacterMovementScript.cs
+++ GD2023/Assets/Rob/Scripts/CharacterMovementScript.cs
@@ -1,33 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterMovementScript : MonoBehaviour
 {
 
-    float walkingSpeed = 3;
-    private float turningSpeed = 90;
+    float currentSpeed = 2;
+    float walkingSpeed = 2;
+    float runningSpeed = 4;
+    private float turningSpeed = 130;
+    Animator edAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        edAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = runningSpeed;
+            edAnimator.SetBool("isRunning", true);
+        }
+        else
+        {
+            currentSpeed = walkingSpeed;
+            edAnimator.SetBool("isRunning", false);
+        }
+
+        edAnimator.SetBool("isWalking", false);
+
         if (Input.GetKey(KeyCode.W))
         {   ///   s            =         u                      *       t
-            transform.position +=walkingSpeed * transform.forward * Time.deltaTime; ;
+            transform.position += currentSpeed * transform.forward * Time.deltaTime;
+            edAnimator.SetBool("isWalking", true);
         }
+    
+
+
         if (Input.GetKey(KeyCode.A))
-        {   
-            transform.Rotate(Vector3.up, -turningSpeed*Time.deltaTime);
+        {
+            transform.Rotate(Vector3.up, -turningSpeed * Time.deltaTime);
+            transform.position += currentSpeed * transform.forward * Time.deltaTime;
+
+            edAnimator.SetBool("isWalking", true);
+        }
+    
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
+            transform.position += currentSpeed * transform.forward * Time.deltaTime;
+
+            edAnimator.SetBool("isWalking", true);
         }
 
 
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= currentSpeed * transform.forward * Time.deltaTime;
 
+            edAnimator.SetBool("isWalkingBackwards", true);
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                currentSpeed = runningSpeed;
+                edAnimator.SetBool("isRunningBackwards", true);
+            }
+            else
+            {
+                currentSpeed = walkingSpeed;
+                edAnimator.SetBool("isRunningBackwards", false);
+            }
+        }
+        else
+        {
+            edAnimator.SetBool("isWalkingBackwards", false);
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            edAnimator.SetBool("pickUP", true);
+        }
+        else 
+        { 
+            edAnimator.SetBool("pickUP", false);
+        }
 
     }
 }
