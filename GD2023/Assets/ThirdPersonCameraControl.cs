aa2c549ef0b606d35aebb2f72485515c2f45e58b
LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>
2023-09-26 16:04:19 +0100

    Signed-off-by: LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>

---
+++ GD2023/Assets/ThirdPersonCameraControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraControl : MonoBehaviour
+{
+    public float desiredAngle = 0;
+    private float bufferZone = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localRotation.eulerAngles.y < desiredAngle - bufferZone)
+        {
+            print("Adding 1");
+            transform.RotateAround(transform.parent.position, Vector3.up, 1);
+        }
+        else
+         if (transform.localRotation.eulerAngles.y > desiredAngle + bufferZone)
+        {
+            print("Subtracting");
+            print("Euler y is " + transform.localRotation.eulerAngles.y.ToString());
+            print("Desired Angle " + desiredAngle.ToString() +
+                " + buffer of " + bufferZone.ToString());
+
+            transform.RotateAround(transform.parent.position, Vector3.up, -1);
+        }
+        
+    
+    }
+}
