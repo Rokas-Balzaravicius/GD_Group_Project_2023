1021d10b3adfc9977270d4ce974da822c99d2153
STUDENT John J. O Sullivan <John.J.OSullivan@students.ittralee.ie>
2023-10-05 13:01:25 +0100

    Signed-off-by: STUDENT John J. O Sullivan <John.J.OSullivan@students.ittralee.ie>

---
+++ GD2023/Assets/NatterJohn/Scripts/RockResourceScript.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting.FullSerializer;
+using UnityEditor;
+using UnityEngine;
+
+internal class RockResourceScript : MonoBehaviour
+{ 
+
+    float TIME_TO_COLLECT = 3;
+    float timer;
+
+    enum ResourceState { Idle, BeingHarvested }
+
+    int typeId = 4; // 4 is rock
+    int quantityInNode = 55;
+    ResourceState currentState = ResourceState.Idle;
+
+
+    MoveJammo harvesterJammo;
+
+    internal void ImHarvestingYou(MoveJammo harvester)
+    {
+        print("Yikes I'm being harvested");
+        harvesterJammo = harvester;
+        startHarvestProcess();
+    }
+
+    private void startHarvestProcess();
+    {
+        currentState = ResourceState.BeingHarvested;
+        timer = TIME_TO_COLLECT;
+
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+}
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        switch(currentState)
+        {
+            case ResourceState.BeingHarvested:
+
+                timer -= Time.deltaTime;
+                if (timer < 0)
+                { 
+                    harvesterJammo.give(quantityInNode, typeId)
+                    theManager.ImHarvestingYou(this);
+                    Destroy(gameObject);
+                }
+                if (harvesterJammo)
+                {
+                currentState = ResourceState.info
+                endVisualEffect();
+                }
+
+
+                break;
+
+            case ResourceState.Idle:
+
+                break;
+        }
+    }
