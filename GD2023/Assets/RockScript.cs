055897a4354c9e48e285133ae70aa98d8b918bed
LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>
2023-10-05 13:01:15 +0100

    Signed-off-by: LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>

--- GD2023/Assets/RockScript.cs
+++ GD2023/Assets/RockScript.cs
@@ -5,20 +5,46 @@ using UnityEngine;
 
 public class RockScript : MonoBehaviour
 {
+    float TIME_TO_COLLECT = 3;
+    float timer;
+
+    enum ResourceState { Idle, BeingHarvested}
+
+    int typeId = 4;  // where 4 represents rock
+    int quantityInNode = 55;
+    ResourceState currentState = ResourceState.Idle;
+
+
     CharacterMovementScript charWhoIsHarvestingMe;
+    ResourceManager theManager;
 
     internal void ImHavestingYou(CharacterMovementScript harvester)
     {
         print("Yikes Im being harvested");
+       
         charWhoIsHarvestingMe = harvester;
         startHarvestProcess();
     }
 
     private void startHarvestProcess()
     {
+        currentState = ResourceState.BeingHarvested;
         // start Timer
+        timer = TIME_TO_COLLECT;
+
+        startVisualEffect();
+    }
+
+    private void startVisualEffect()
+    {
         // start Visual effect
-        GetComponent<Renderer>().material.color = Color.green;  ;
+        GetComponent<Renderer>().material.color = Color.green; ;
+    }
+
+    private void endVisualEffect()
+    {
+        // start Visual effect
+        GetComponent<Renderer>().material.color = Color.white; ;
     }
 
     // Start is called before the first frame update
@@ -30,6 +56,44 @@ public class RockScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch(currentState)
+        {
+            case ResourceState.BeingHarvested:
+
+                timer -= Time.deltaTime;
+                if (timer < 0)
+                {
+                    // give the harvester the appropriate rescouces
+                    charWhoIsHarvestingMe.give(quantityInNode, typeId);
+                    theManager.IveBeenHarvested(this);
+                    Destroy(gameObject);
+                }
+
+                if (charWhoIsHarvestingMe.currentlyIAm != 
+                                CharacterMovementScript.characterState.Havesting)
+                {
+                    currentState = ResourceState.Idle;
+                    endVisualEffect();
+
+                }
+
+
+
+                break; 
+            
+            case ResourceState.Idle:
+
+
+                break;
+
+        }
+
         
+    }
+
+    internal void IamYourManager(ResourceManager resourceManager)
+    {
+        theManager = resourceManager; 
+       
     }
 }
