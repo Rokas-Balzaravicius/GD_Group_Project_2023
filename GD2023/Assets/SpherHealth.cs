a40f47d258544b43c1b49e7c3170eaf09259be17
STUDENT Brandon C. Mulpeter <Brandon.Mulpeter@students.ittralee.ie>
2023-10-19 12:51:52 +0100

    Signed-off-by: STUDENT Brandon C. Mulpeter <Brandon.Mulpeter@students.ittralee.ie>

--- GD2023/Assets/SpherHealth.cs
+++ GD2023/Assets/SpherHealth.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pickupEquip : MonoBehaviour
+public class SpherHealth : MonoBehaviour
 {
-
-    Transform rightHand;
     // Start is called before the first frame update
     void Start()
     {
-      //  foreach (Transform childTransform in RightHandCarry.transform) ;
+        
     }
 
     // Update is called once per frame
