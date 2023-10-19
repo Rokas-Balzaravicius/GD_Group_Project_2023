3f42c64be04c8da1168f76427e8b84770c089980
STUDENT John J. O Sullivan <John.J.OSullivan@students.ittralee.ie>
2023-10-19 13:04:15 +0100

    Signed-off-by: STUDENT John J. O Sullivan <John.J.OSullivan@students.ittralee.ie>

---
+++ GD2023/Assets/NatterJohn/Scripts/CursorObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorObject : MonoBehaviour
+{
+
+    [SerializeField] private CursorManager.CursorType cursorType;
+    private void OnMouseEnter() {
+        
+    }
+
+    private void OnMouseExit()
+    {
+
+    }
+}
