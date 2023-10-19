f4778073e7805c77e95b0953f7557f80d67cb429
STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>
2023-10-19 13:04:26 +0100

    Signed-off-by: STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>

--- GD2023/Assets/Anna/Scripts/Tooltip.cs
+++ GD2023/Assets/Anna/Scripts/Tooltip.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tooltip : MonoBehaviour
 {
-    public string message;
+    private string message;
+    Transform ownerTransform;
+
+    internal void assignMessage(string newMessage)
+    {
+       message = newMessage;
+    }
+
+    internal void assignMessage(string mesasge, Transform transformOfOwner)
+    {
+       ownerTransform = transformOfOwner;
+       TooltipManager.instance.SetAndShowToolTip(message,ownerTransform);
+    }
 
     private void OnMouseEnter()
-    {
+    { 
         TooltipManager.instance.SetAndShowToolTip(message);
     }
 
     private void OnMouseExit()
     {
-        TooltipManager.instance.HideTooltip();
+      //  TooltipManager.instance.HideTooltip();
     }
 }
