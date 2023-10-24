1bc92665af9680b9060b17501d9acf0802a36967
STUDENT Rokas Balzaravicius <Rokas.Balzaravicius@students.ittralee.ie>
2023-10-24 15:47:50 +0100

    Signed-off-by: STUDENT Rokas Balzaravicius <Rokas.Balzaravicius@students.ittralee.ie>

--- GD2023/Assets/Rokas/Scripts/WeaponSwap.cs
+++ GD2023/Assets/Rokas/Scripts/WeaponSwap.cs
@@ -6,39 +6,27 @@ using UnityEngine;
 public class WeaponSwap : MonoBehaviour
 {
     Transform currentlyHolding;
-    public GameObject[] AdjustedAxe, Sword1Adjusted;
+
     private int currentWeaponIndex = 0;
 
     void Start()
     {
-        SwitchWeapon(currentWeaponIndex);
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha1)) 
-        {
-            SwitchWeapon(0);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            SwitchWeapon(1);
-        }
+
     }
 
-    void SwitchWeapon(int weaponIndex)
-    {
-        AdjustedAxe[currentWeaponIndex].SetActive(false);
 
-        currentWeaponIndex = weaponIndex;
-        AdjustedAxe[currentWeaponIndex].SetActive(true);
-    }
 
     internal void equip(PickUP newItem)
     {
         if (currentlyHolding !=  null)
         {
+            print("1");
 
             currentlyHolding.transform.parent = null;
             currentlyHolding.position = transform.position;
@@ -47,7 +35,7 @@ public class WeaponSwap : MonoBehaviour
 
 
         }
-
+        print("2");
         newItem.transform.parent = transform;
         newItem.transform.localPosition = Vector3.zero;
         newItem.transform.localRotation = Quaternion.identity;
