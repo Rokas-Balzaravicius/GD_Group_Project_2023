8ea2e594d7f429efd73d1d66c955899c1d1071ea
STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>
2023-10-24 15:58:03 +0100

    Signed-off-by: STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>

--- GD2023/Assets/Anna/Scripts/TooltipManager.cs
+++ GD2023/Assets/Anna/Scripts/TooltipManager.cs
@@ -8,11 +8,22 @@ using Unity.VisualScripting;
 public class TooltipManager : MonoBehaviour
 {
    
-    public static TooltipManager instance;
+    public static  TooltipManager instance;
     public TextMeshProUGUI textComponent;
 
     public RectTransform rectTransform;
     private Transform ownerTransform;
+
+    public GameObject interactiveObject;
+
+    public Transform characterTransform; // Reference to the character's Transform.
+
+    //public Vector3 offset = new Vector3(0f, 2f, 0f);
+    //public Vector3 characterWorldPosition; // Reference to the character's position.
+    //public Vector3 tooltipScreenPosition;
+
+    public float interactDistanceThreshold = 1.0f; // The distance at which the tooltip appears.
+    private bool isNearObject = false;
 
     private void Awake()
     {
@@ -45,15 +56,63 @@ public class TooltipManager : MonoBehaviour
             print("Screen Position " + position.ToString());
 
         }
-        else
-            position = Input.mousePosition;
-            transform.position = position;
-
-        float pivotX = position.x / Screen.width;
-        float pivotY = position.y / Screen.height;
-
-        rectTransform.pivot = new Vector3(pivotX, pivotY);
         
+        
+        
+        position = Input.mousePosition;
+        Ray ourRay = Camera.main.ScreenPointToRay(position);
+        RaycastHit info;
+
+        if (Physics.Raycast(ourRay, out info))
+        {
+            print("Hit Object Name: " + info.collider.gameObject.name);
+            if (characterTransform == interactiveObject.transform)
+            {
+    
+                float distance = Vector3.Distance(characterTransform.position, interactiveObject.transform.position); // Calculate the distance between the character and the interactive object.
+
+                if (distance < interactDistanceThreshold)
+                {
+                    isNearObject = true;
+                    print("You are near the object.");
+                }
+                else
+                {
+                    isNearObject = false;
+                    print("You are not near the object.");
+                }
+            }
+        }
+
+        //try to display tooltip above the character
+        //if (characterTransform)
+        //{
+           // characterWorldPosition = characterTransform.position; // Update the character's world position.
+
+           // Vector3 targetWorldPosition = characterWorldPosition + offset; // Calculate the fixed world position of the tooltip relative to the character.
+
+           // tooltipScreenPosition = Camera.main.WorldToScreenPoint(targetWorldPosition); // Convert the world position to screen coordinates.
+
+           // transform.position = tooltipScreenPosition; // Set the tooltip's position in screen space.
+       // }
+
+
+        
+
+
+
+
+
+
+
+        //transform.position = position;
+
+        //check pivotx and pivoty of window and change position if it is close to the end of the screen.
+        // float pivotX = position.x / Screen.width;
+        //float pivotY = position.y / Screen.height;
+
+        // rectTransform.pivot = new Vector3(pivotX, pivotY);
+
     }
 
     public void SetAndShowToolTip(string message)
