85116c56178413fbcb292e2cd8fc05d8f3b83860
Kastrel7 <jazzjunior1@gmail.com>
2023-11-21 06:04:01 +0000

    Signed-off-by: Kastrel7 <jazzjunior1@gmail.com>

--- GD2023/Assets/Kass/Scripts/KMapScript.cs
+++ GD2023/Assets/Kass/Scripts/KMapScript.cs
@@ -1,12 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class KMapScript : MonoBehaviour
 {
-    Vector3 offset = new Vector3(70, 70, 0);
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +14,12 @@ public class KMapScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 offset = new Vector3(70, 70, 0);
         Transform parentTransform = transform.parent;
-        transform.localScale = new Vector3(parentTransform.localScale.x / 1920, parentTransform.localScale.y / 1080, 1);
-        Vector3 newSize = new Vector3((parentTransform.localScale.x/2), -(parentTransform.localScale.y/2), 0);
-        Vector3 newPos = newSize - offset;
+        //transform.localScale = new Vector3(parentTransform.localScale.x / 1920, parentTransform.localScale.y / 1080, 1);
+        Vector3 newSize = new Vector3((parentTransform.localScale.x / 2), -(parentTransform.localScale.y / 2), 0);
+        Vector3 newPos = (newSize - offset);
+        print(newPos);
         transform.position = newPos;
     }
 }
