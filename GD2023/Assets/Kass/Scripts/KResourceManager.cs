1bd6eac70c415b9b05e3638ce6b4f309c5e7eb20
Kastrel7 <jazzjunior1@gmail.com>
2023-11-21 15:54:03 +0000

    Signed-off-by: Kastrel7 <jazzjunior1@gmail.com>

--- GD2023/Assets/Kass/Scripts/KResourceManager.cs
+++ GD2023/Assets/Kass/Scripts/KResourceManager.cs
@@ -5,18 +5,45 @@ using UnityEngine;
 public class KResourceManager : MonoBehaviour
 {
 
+    List<KRockScript> allRocks;
+    List<KTreeScript> allTrees;
     public Transform rockCloneTemplate;
+    public Transform treeCloneTemplate;
 
     int NUMBER_OF_ROCKS = 100;
 
     // Start is called before the first frame update
     void Start()
     {
+        allRocks = new List<KRockScript>();
+        allTrees = new List<KTreeScript>();
+
         for (int i = 0; i < NUMBER_OF_ROCKS; i++)
         {
-            Instantiate(rockCloneTemplate, getRockSpawnLocation(), Quaternion.identity);
-            print(i);
+            spawnCloneinRandomLocation(rockCloneTemplate);
+            spawnCloneinRandomLocation(treeCloneTemplate);
         }
+
+    }
+
+    private void spawnCloneinRandomLocation(Transform clone)
+    {
+        Transform newClonetf = Instantiate(clone, getRockSpawnLocation(), Quaternion.identity);
+
+
+        KRockScript newRock = newClonetf.GetComponent<KRockScript>();
+        if (newRock != null)
+        {
+            newRock.IamYourManager(this);
+            allRocks.Add(newRock);
+        }
+        KTreeScript newTree = newClonetf.GetComponent<KTreeScript>();
+        if (newTree != null)
+        {
+            newTree.IamYourManager(this);
+            allTrees.Add(newTree);
+        }
+
     }
 
     private Vector3 getRockSpawnLocation()
@@ -27,6 +54,17 @@ public class KResourceManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    internal void IveBeenHarvested(KHarvestableItem harvestScript)
+    {
+        print("Spawning new Rock");
+
+        // remove from list
+        if (harvestScript is KRockScript)
+            allRocks.Remove(harvestScript as KRockScript);
+
+
     }
 }
